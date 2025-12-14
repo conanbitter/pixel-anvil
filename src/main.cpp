64ec64a6496e0413d1caf82286f961998e17e547
Conan Bitter <conan.bitter@gmail.com>
2025-12-14 13:02:00 +0300

    Scaffold glfw and OpenGL

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include <cstdio>
+
+int main(void) {
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+    glfwWindowHint(GLFW_POSITION_X, (mode->width - 640) / 2);
+    glfwWindowHint(GLFW_POSITION_Y, (mode->height - 480) / 2);
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    int version = gladLoadGL(glfwGetProcAddress);
+    if (version == 0) {
+        printf("Failed to initialize OpenGL context\n");
+        return -1;
+    }
+
+    printf("Loaded OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+
+    glClearColor(0.09, 0.14, 0.16, 1.0);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
