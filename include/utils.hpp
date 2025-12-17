e5a2436ff7ded8dbe8c356e059f6cf0b08b3fcd9
Conan Bitter <conan.bitter@gmail.com>
2025-12-17 21:54:58 +0300

    Add Color type

--- include/utils.hpp
+++ include/utils.hpp
@@ -73,4 +73,32 @@ namespace pixanv {
         FSize size() const { return FSize(width, height); }
     };
 
+#pragma pack(push, 1) 
+    class Color {
+    public:
+        Color() :value(0) {}
+        Color(uint16_t raw) :value(raw) {}
+        Color(int r, int g, int b, int a);
+        int r() const;
+        int g() const;
+        int b() const;
+        int a() const;
+
+        static const Color BLACK;
+        static const Color WHITE;
+        static const Color RED;
+        static const Color GREEN;
+        static const Color BLUE;
+        static const Color CYAN;
+        static const Color MAGENTA;
+        static const Color YELLOW;
+
+        bool operator==(const Color& rhs)const { return value == rhs.value; }
+        Color operator*(const Color& rhs)const;
+        Color operator*(float rhs)const;
+    private:
+        uint16_t value;
+    };
+#pragma pack(pop)
+
 }
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,4 +1,6 @@
 #include "utils.hpp"
+#include <cmath>
+#include <algorithm>
 
 using namespace pixanv;
 
@@ -10,3 +12,65 @@ bool Rect::contains(const Point& point)
         point.x < x + width &&
         point.y < y + height;
 }
+
+#pragma region Color
+
+Color::Color(int r, int g, int b, int a = 1)
+{
+    a = std::clamp(a, 0, 1);
+    r = std::clamp(r, 0, 0b11111);
+    g = std::clamp(g, 0, 0b11111);
+    b = std::clamp(b, 0, 0b11111);
+    value = a << 15 | r << 10 | g << 5 | b;
+}
+
+int Color::r() const
+{
+    return (value >> 10) & 0b11111;
+}
+
+int Color::g() const
+{
+    return (value >> 5) & 0b11111;
+}
+
+int Color::b() const
+{
+    return value & 0b11111;
+}
+
+int Color::a() const
+{
+    return value >> 15;
+}
+
+Color pixanv::Color::operator*(const Color& rhs) const
+{
+    return Color(
+        (r() * rhs.r()) / 31,
+        (g() * rhs.g()) / 31,
+        (b() * rhs.b()) / 31,
+        (a() * rhs.a())
+    );
+}
+
+Color pixanv::Color::operator*(float rhs) const
+{
+    return Color(
+        std::round(r() * rhs),
+        std::round(g() * rhs),
+        std::round(b() * rhs),
+        a()
+    );
+}
+
+const Color Color::BLACK = Color(0, 0, 0);
+const Color Color::WHITE = Color(31, 31, 31);
+const Color Color::RED = Color(31, 0, 0);
+const Color Color::GREEN = Color(0, 31, 0);
+const Color Color::BLUE = Color(0, 0, 31);
+const Color Color::CYAN = Color(0, 31, 31);
+const Color Color::MAGENTA = Color(31, 0, 31);
+const Color Color::YELLOW = Color(31, 31, 0);
+
+#pragma endregion Color
