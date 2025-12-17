262c2a986b47da9b4db1764fb49b316658769d8e
Conan Bitter <conan.bitter@gmail.com>
2025-12-17 22:03:47 +0300

    Add Image class

---
+++ include/image.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "utils.hpp"
+#include <vector>
+
+using std::vector;
+
+namespace pixanv {
+
+    class Image
+    {
+    public:
+        Image(int width, int height) :m_width(width), m_height(height), m_data(width* height) {}
+        bool isEmpty() const { return m_width == 0 || m_height == 0; }
+
+        void pixel(int x, int y, Color color);
+        Color pixel(int x, int y) const;
+        void fill(Color color);
+
+        int width() const { return m_width; };
+        int height() const { return m_height; };
+    protected:
+        Image() :m_width(0), m_height(0), m_data(0) {};
+        int m_width;
+        int m_height;
+        vector<Color> m_data;
+
+        void pixelRaw(int x, int y, Color color);
+        Color pixelRaw(int x, int y) const;
+    };
+
+}
